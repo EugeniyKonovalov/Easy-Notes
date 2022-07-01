c6df2eecb34611d3fb4661c204d586563f90709b
EvgeniyKonovalov <cicerron2020@gmail.com>
2022-07-01 11:38:08 +0300

    add auth

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Card from "../components/UI/Card";
+import { useAuth } from "../hooks/useAuth";
 
 const Home = () => {
+  const { isAuth } = useAuth();
   return (
     <Card>
-      <h2 className="home">Keep your notes in order!</h2>
+      <h2 className="home">
+        Keep your notes in order!
+        {!isAuth && (
+          <p>If you want to add folders or notes, you need to login!</p>
+        )}
+      </h2>
     </Card>
   );
 };
